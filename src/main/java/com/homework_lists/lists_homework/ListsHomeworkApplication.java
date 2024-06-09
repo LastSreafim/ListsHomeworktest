2fba691fb5fbc671b58061cc02100d7a9c9b1fb3
Nazar <epoxasmute@mail.ru>
2024-06-09 20:21:01 +0300

    GitInit

---
+++ src/main/java/com/homework_lists/lists_homework/ListsHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.homework_lists.lists_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListsHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListsHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homework_lists/lists_homework/ListsHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework_lists.lists_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
